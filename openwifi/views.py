f1fec85bd735140a5ed936421e49e09ce1e7e759
Alexander Couzens <lynxis@fe80.eu>
2013-07-29 01:29:11 +0200

    add home template

--- openwifi/views.py
+++ openwifi/views.py
@@ -5,17 +5,12 @@ from sqlalchemy.exc import DBAPIError
 
 from .models import (
     DBSession,
-    MyModel,
     )
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
-def my_view(request):
-    try:
-        one = DBSession.query(MyModel).filter(MyModel.name == 'one').first()
-    except DBAPIError:
-        return Response(conn_err_msg, content_type='text/plain', status_int=500)
-    return {'one': one, 'project': 'openwifi'}
+@view_config(route_name='home', renderer='templates/home.pt')
+def home(request):
+    return {}
 
 conn_err_msg = """\
 Pyramid is having a problem using your SQL database.  The problem
