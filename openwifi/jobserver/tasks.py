94f8f1de69a41759d6336f0637c1e3722f77b36f
Johannes Wegener <mail@johanneswegener.de>
2015-03-04 13:55:20 +0100

    celery: get_config is put into try except

--- openwifi/jobserver/tasks.py
+++ openwifi/jobserver/tasks.py
@@ -31,25 +31,28 @@ app.conf.CELERY_TIMEZONE = 'UTC'
 
 @app.task
 def get_config(uuid):
-    engine = create_engine(sqlurl)
-    Session = sessionmaker()
-    Session.configure(bind=engine)
-    DBSession=Session()
-    device = DBSession.query(OpenWrt).get(uuid)
-    device_url = "http://"+device.address+"/ubus"
-    js = jsonubus.JsonUbus(url=device_url, user=device.login, password=device.password)
-    device_configs = js.call('uci', 'configs')
-    device.configuration="{"
-    for cur_config in device_configs[1]['configs']:
-        device.configuration+='"'+cur_config+'":'+str(js.call("uci","get",config=cur_config)[1])+","
-    device.configuration=device.configuration[:-1]+"}"
-    device.configuration=device.configuration.replace("True", "'true'")
-    device.configuration=device.configuration.replace("False", "'false'")
-    device.configuration=device.configuration.replace("'",'"')
-    device.configured = True
-    DBSession.commit()
-    DBSession.close()
-    return True
+    try:
+        engine = create_engine(sqlurl)
+        Session = sessionmaker()
+        Session.configure(bind=engine)
+        DBSession=Session()
+        device = DBSession.query(OpenWrt).get(uuid)
+        device_url = "http://"+device.address+"/ubus"
+        js = jsonubus.JsonUbus(url=device_url, user=device.login, password=device.password)
+        device_configs = js.call('uci', 'configs')
+        device.configuration="{"
+        for cur_config in device_configs[1]['configs']:
+            device.configuration+='"'+cur_config+'":'+str(js.call("uci","get",config=cur_config)[1])+","
+        device.configuration=device.configuration[:-1]+"}"
+        device.configuration=device.configuration.replace("True", "'true'")
+        device.configuration=device.configuration.replace("False", "'false'")
+        device.configuration=device.configuration.replace("'",'"')
+        device.configured = True
+        DBSession.commit()
+        DBSession.close()
+        return True
+    except:
+        return False
 @app.task
 def update_config(uuid, config):
     engine = create_engine(sqlurl)
