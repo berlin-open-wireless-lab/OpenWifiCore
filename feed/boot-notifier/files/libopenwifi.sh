bf64a358aa80176f9b43b10e0b36836cdc79b921
Alexander Couzens <lynxis@fe80.eu>
2015-05-04 03:21:20 +0200

    boot-notifier: finish openwifi()

--- feed/boot-notifier/files/libopenwifi.sh
+++ feed/boot-notifier/files/libopenwifi.sh
@@ -4,6 +4,12 @@
 
 PROTOCOL=0.1
 
+_log() {
+  local level=$1
+  shift
+  logger -s -t openwifi -p daemon.$level $@
+}
+
 user_del() {
   local user="$1"
   local tmp
@@ -13,12 +19,12 @@ user_del() {
   fi
 
   [ -n "$IPKG_INSTROOT" ] || lock /var/lock/passwd
-  tmp=$(mktemp /tmp/userdel_XXXXXX)
+  tmp=$(mktemp /etc/shadow_XXXXXX)
 
   # remove generatepw user
-  cp /etc/shadow $tmp
+  cp -a /etc/shadow $tmp
   grep -v "^generatepw:" $tmp > /etc/shadow
-  cp /etc/passwd $tmp
+  cp -a /etc/passwd $tmp
   grep -v "^generatepw:" $tmp > /etc/passwd
   rm $tmp
   [ -n "$IPKG_INSTROOT" ] || lock -u /var/lock/passwd
@@ -39,6 +45,16 @@ EOF
 
 # register a device to the controller
 device_register() {
+  local server=$1
+  local uuid=$2
+  local address
+
+  address=$(nslookup "$server" 2>/dev/null | tail -n1 | awk '{print $3}')
+  if [ -z "$address" ] ; then
+    _log error "Could not find server"
+    return 1
+  fi
+
   . /etc/openwrt_release
 
   localaddress=$(ip r g "${address}" | head -n1 | awk '{ print $5 }')
@@ -50,6 +66,8 @@ device_register() {
   cryptpw="$(grep ^generatepw: /etc/shadow | awk -F: '{print $2}')"
   user_del generatepw
   uci set rpcd.@login[0].password="$cryptpw"
+  uci commit rpcd
+  _log info "Registering to server $server"
 
   wget -q -O/dev/null \
       --header='Content-Type: application/json' \
@@ -71,8 +89,9 @@ device_register() {
 }
 
 # check if device is already registered
-device_check_registered() {
-  local uuid="$1"
+device_is_registered() {
+  local server="$1"
+  local uuid="$2"
 
   RESPONSE=$(wget -q -O- \
       --header='Content-Type: application/json' \
@@ -81,9 +100,18 @@ device_check_registered() {
           { \
             \"uuid\":\"${uuid}\", \
           } \
-        \"method\": \"device_register\", \
+        \"method\": \"device_is_registered\", \
         \"jsonrpc\": \"2.0\" }" \
       "http://${address}/api")
+
+  json_load "$RESPONSE"
+  json_get_var result result
+
+  if [ "$result" = "yes" ] ; then
+    return 1;
+  else
+    return 0;
+  fi
 }
 
 # check if server $1 is a openwifi server
@@ -113,7 +141,7 @@ device_discover_server() {
 # search for a openwifi controller and set it if found
 device_discover() {
   if device_discover_server "openwifi" ; then
-    set_controller openwifi
+    set_controller "openwifi"
     return 0
   fi
 
@@ -134,20 +162,77 @@ device_discover() {
   return 1
 }
 
-set_controller() {
-  local server
-  server="$1"
+device_generate_uuid() {
+  local uuid=""
 
-  uci delete openwifi.@server[]
-  uci add openwifi server
-  uci set openwifi.@server[0].server="$server"
-
-  if ! device_register "$server" ; then
-    # device register failed!
-    uci revert openwifi
+  uuid=$(cat /proc/sys/kernel/random/uuid)
+  if [ -z "$uuid" ] ; then
     return 1
   fi
 
+  uci set openwifi.@device[0].uuid="$uuid"
   uci commit openwifi
   return 0
+}
+
+# try to set the controller and register to it
+set_controller() {
+  local server=$1
+  local uuid=$(uci get openwifi.@device[0].uuid)
+
+  if ! device_register "$server" "$uuid" ; then
+    return 1
+  fi
+
+  uci delete openwifi.@server[]
+  uci add openwifi server
+  uci set openwifi.@server[0].address="$server"
+  uci commit openwifi
+  return 0
+}
+
+openwifi() {
+  local server
+  local uuid
+  local i=0
+
+  while [ $i -lt 3 ] ; do
+    server=$(uci get openwifi.@server[0].address)
+    uuid=$(uci get openwifi.@device[0].uuid)
+
+    # check if a uuid was generated
+    if [ -z "$uuid" ] ; then
+      if ! device_generate_uuid ; then
+        _log error "Could not generate a uuid"
+        continue
+      fi
+      uuid=$(uci get openwifi.@device[0].uuid)
+    fi
+
+    if [ -z "$server" ] ; then
+      if ! device_discover ; then
+        _log error "Could not discover a server"
+        continue
+      fi
+      server=$(uci get openwifi.@server[0].address)
+    fi
+
+    # check if server is reachable
+    if ! device_discover_server $server ; then
+      _log error "Server $server does not respond! Clear old server"
+      uci delete openwifi.@server[]
+      uci commit openwifi
+      continue
+    fi
+
+    if ! device_is_registered "$server" "$uuid" ; then
+      device_register "$server" "$uuid" && return 0
+    else
+      return 0
+    fi
+    i=$((i + 1))
+    sleep 3
+  done
+  _log error "Could not find a suitable server or server doesn't repond"
+  return 1
 }
