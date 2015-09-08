a3a07e9d961516201a212eda99626c9263e9402d
Johannes Wegener <mail@johanneswegener.de>
2015-09-08 12:25:22 +0200

    more tests about changing and deleting options

--- openwifi/jobserver/testTemplates.py
+++ openwifi/jobserver/testTemplates.py
@@ -106,6 +106,91 @@ generateConfig="""
 }
 """
 
+deleteFirstOption = """
+{
+    "metaconf": {
+        "change": {
+            "del": [],
+            "add": []
+        },
+        "packages": [
+            {
+                "matchvalue": "TestPackage",
+                "change": {
+                    "del": [],
+                    "add": []
+                },
+                "config": [
+                    {
+                        "change": {
+                            "del": [
+                                "1st Option"
+                            ],
+                            "add": []
+                        },
+                        "matchtype": "name",
+                        "matchcount": "",
+                        "matchvalue": "TestConfig",
+                        "ucitype": "blubb",
+                        "next": "",
+                        "type": "config"
+                    }
+                ],
+                "type": "package"
+            }
+        ]
+    }
+}
+"""
+
+changeFirstOption = """
+{
+    "metaconf": {
+        "packages": [
+            {
+                "change": {
+                    "del": [],
+                    "add": [
+                        [
+                            "Something",
+                            "TestConfigType",
+                            "type"
+                        ]
+                    ]
+                },
+                "matchvalue": "TestPackage",
+                "type": "package",
+                "config": [
+                    {
+                        "matchtype": "type",
+                        "matchcount": "",
+                        "next": "",
+                        "matchvalue": "Something",
+                        "type": "config",
+                        "ucitype": "TestConfigType",
+                        "change": {
+                            "del": [],
+                            "add": [
+                                [
+                                    "1st Option",
+                                    "Something Else"
+                                ]
+                            ]
+                        }
+                    }
+                ]
+            }
+        ],
+        "change": {
+            "del": [],
+            "add": [
+                "TestPackage"
+            ]
+        }
+    }
+}
+"""
+
 class TestSetup(unittest.TestCase):
     def setUp(self):
         pass
@@ -117,5 +202,17 @@ class TestSetup(unittest.TestCase):
     def testGenereateConfig(self):
         expected_answer = '{"TestPackage": {"values": {"SecondConfig": {"2nd Config 1st Option": "2nd Config 1st Option Value", ".anonymous": false, ".name": "SecondConfig", ".type": "SecondConfigType"}, "TestConfig": {"1st Option": "Option Value 1", ".anonymous": false, ".name": "TestConfig", "2nd Option": "Option Value 2", ".type": "TestConfigType"}}}}'
         answer = update_template(emptyConfig,generateConfig)
+        assert(json.loads(answer.export_json())==json.loads(expected_answer))
+    def testDelOption(self):
+        expected_answer = '{"TestPackage": {"values": {"SecondConfig": {"2nd Config 1st Option": "2nd Config 1st Option Value", ".anonymous": false, ".name": "SecondConfig", ".type": "SecondConfigType"}, "TestConfig": {".anonymous": false, ".name": "TestConfig", "2nd Option": "Option Value 2", ".type": "TestConfigType"}}}}'
+        answer = update_template(emptyConfig,generateConfig)
+        answer = update_template(answer.export_json(), deleteFirstOption)
+        assert(json.loads(answer.export_json())==json.loads(expected_answer))
+    def testChangeOption(self):
+        expected_answer = '{"TestPackage": {"values": {"SecondConfig": {"2nd Config 1st Option": "2nd Config 1st Option Value", ".anonymous": false, ".name": "SecondConfig", ".type": "SecondConfigType"}, "TestConfig": {"1st Option": "Something Else", ".anonymous": false, ".name": "TestConfig", "2nd Option": "Option Value 2", ".type": "TestConfigType"}}}}'
+        answer = update_template(emptyConfig,generateConfig)
+        answer = update_template(answer.export_json(),changeFirstOption)
+        pp = pprint.PrettyPrinter(indent=4)
+        pp.pprint(json.loads(answer.export_json()))
         assert(json.loads(answer.export_json())==json.loads(expected_answer))
     # TODO: Add more tests
