174d3fc5d092903709cf3fac086f3dfd23519972
Johannes Wegener <mail@johanneswegener.de>
2016-02-09 17:39:53 +0100

    vagrant: add dhcp and mdns

--- Vagrant/bootstrap.sh
+++ Vagrant/bootstrap.sh
@@ -112,3 +112,18 @@ chown -R www-data /var/www/html
 
 #cp /vagrant/mpm_prefork.conf /etc/apache2/mods-available/
 service apache2 restart
+
+
+# install avahi
+apt-get -y install avahi-daemon
+cp /vagrant/openwifi.service /etc/avahi/services/openwifi.service
+/etc/init.d/avahi-daemon restart
+
+# use dnsmasq as dhcp server
+apt-get -y dnsmasq
+echo "interface=eth1" >> /etc/dnsmasq.conf
+echo "bind-interfaces" >> /etc/dnsmasq.conf
+echo "dhcp-range=192.168.50.100,192.168.50.254,12h" >> /etc/dnsmasq.conf
+/etc/init.d/dnsmasq restart
+
+
