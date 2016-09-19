12db824b9cd663ac31134958bbbc6f57e6db469a
Johannes Wegener <mail@johanneswegener.de>
2016-09-19 17:00:38 +0200

    Docker: fix run_ldap

--- Docker/DockerInstall/run_ldap.sh
+++ Docker/DockerInstall/run_ldap.sh
@@ -6,5 +6,8 @@ if [ ! -f /etc/ldap/slapd.d/cn=config.ldif ]; then
   /DockerInstall/install_ldap.sh
 fi
 
-exec /usr/sbin/slapd -F /etc/ldap/slapd.d -h "ldapi:/// ldap:/// ldaps:///" -d stats
+#exec /usr/sbin/slapd -F /etc/ldap/slapd.d -h "ldapi:/// ldap:/// ldaps:///" -d stats
+mkdir -p /var/run/slapd
+touch /var/run/slapd/slapd.pid
+exec /usr/sbin/slapd -F /etc/ldap/slapd.d -h "ldap:/// ldaps:///" -d stats
 
